b4c0f2dce658b258b59f0d387a627ace9ba90858
Aaron Horler <git@aaronhorler.com>
2018-05-08 15:57:44 +1000

    Indentation changes, switch to port 5354, and support IPv6.

--- common/service.sh
+++ common/service.sh
@@ -8,16 +8,17 @@ MODDIR=${0%/*}
 
 while true
 do
-	ping -c 1 download.dnscrypt.info
-	if [[ $? == 0 ]];
-	then
-		
-		$MODDIR/system/xbin/dnscrypt-proxy -config $MODDIR/system/etc/dnscrypt-proxy/dnscrypt-proxy.toml &
-		sleep 5
-		iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5353
-		iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5353
-		break;
-	else
-		sleep 5
-	fi
+  ping -c 1 cloudflare-dns.com
+  if [[ $? == 0 ]];
+  then
+    $MODDIR/system/xbin/dnscrypt-proxy -config $MODDIR/system/etc/dnscrypt-proxy/dnscrypt-proxy.toml &
+    sleep 5
+    iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+    iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+    ip6tables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination [::1]:5354
+    ip6tables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination [::1]:5354
+    break;
+  else
+    sleep 5
+  fi
 done
